96b44553c061dd3e22570d576eae0570c7a17c23
darkdeveloper <goldeagle00110011@outlook.com>
2021-11-19 15:29:47 +0330

    00:08:28
    - Init project

---
+++ train6/src/main/java/com/example/train6/Train6Application.java
@@ -0,0 +1,13 @@
+package com.example.train6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Train6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Train6Application.class, args);
+    }
+
+}
