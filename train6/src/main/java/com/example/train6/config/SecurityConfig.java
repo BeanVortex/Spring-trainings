28a39dcd832886d39c0f1c8f4e2517df09b5ecca
darkdeveloper <goldeagle00110011@outlook.com>
2021-11-21 16:53:47 +0330

    00:08:30
    - JDBC AUTH MANAGER
    - Difference between role and authority is just a 'ROLE_' prefix.
    - any time used hasRole("ADMIN") means hasAuthority("ROLE_ADMIN")

--- train6/src/main/java/com/example/train6/config/SecurityConfig.java
+++ train6/src/main/java/com/example/train6/config/SecurityConfig.java
@@ -1,22 +1,31 @@
 package com.example.train6.config;
 
+import com.example.train6.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService us;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/leaders/**").hasAnyRole("MANAGER")
-                .antMatchers("/").hasAnyRole("EMPLOYEE")
-                .antMatchers("/systems/**").hasAnyRole("ADMIN")
+                .antMatchers("/leaders/**").hasAnyAuthority("ROLE_MANAGER")
+                .antMatchers("/").hasAnyAuthority("ROLE_EMPLOYEE")
+                .antMatchers("/systems/**").hasAnyAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -35,10 +44,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        var user = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(user.username("dd2").password("1234").roles("EMPLOYEE"))
-                .withUser(user.username("dd3").password("1234").roles("EMPLOYEE", "MANAGER"))
-                .withUser(user.username("dd4").password("1234").roles("EMPLOYEE", "ADMIN"));
+//        var user = User.withDefaultPasswordEncoder();
+//        auth.jdbcAuthentication().dataSource(ds)
+//                .usersByUsernameQuery("select username, password, enabled from users where username = ?")
+//                .authoritiesByUsernameQuery("select username, roles from authorities where username = ?");
+//        auth.inMemoryAuthentication()
+//                .withUser(user.username("dd2").password("1234").roles("EMPLOYEE"))
+//                .withUser(user.username("dd3").password("1234").roles("EMPLOYEE", "MANAGER"))
+//                .withUser(user.username("dd4").password("1234").roles("EMPLOYEE", "ADMIN"));
+        auth.userDetailsService(us);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
