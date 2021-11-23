d91908c5ba4b0395a91bde622edb7aa29f0ce282
darkdeveloper <goldeagle00110011@outlook.com>
2021-11-23 23:13:52 +0330

    00:09:02
    - Init proj

---
+++ microservices/train1/src/main/java/com/example/train1/Train1Application.java
@@ -0,0 +1,13 @@
+package com.example.train1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Train1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Train1Application.class, args);
+    }
+
+}
