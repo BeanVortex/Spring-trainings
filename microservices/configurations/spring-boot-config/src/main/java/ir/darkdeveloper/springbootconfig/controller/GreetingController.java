a7ea60882520feb16de52a9581bbff0293f1d815
darkdeveloper <goldeagle00110011@outlook.com>
2021-12-01 00:18:10 +0330

    00:09:10
    - Used dynamic refresh of configs with post request to actuator/refresh

--- microservices/configurations/spring-boot-config/src/main/java/ir/darkdeveloper/springbootconfig/controller/GreetingController.java
+++ microservices/configurations/spring-boot-config/src/main/java/ir/darkdeveloper/springbootconfig/controller/GreetingController.java
@@ -5,6 +5,7 @@ import ir.darkdeveloper.springbootconfig.config.DBSettings;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RefreshScope
 public class GreetingController {
 
     @Value("${app.sad: default val}")
@@ -36,6 +38,10 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greet() {
-        return greet + " " + list + " " + map + " " + dbSettings + "\n\n" + env + "\n\n" + appNameFromCloudSpecificConfig;
+        return appNameFromCloudSpecificConfig;
     }
+
+    // to update configs dynamically do this:
+    // curl -X POST http://localhost:8080/actuator/refresh
+
 }
