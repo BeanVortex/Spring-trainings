ffa017e1235cb1570162073bc0e5ef1f9562a737
DarkDeveloper <goldeagle00110011@outlook.com>
2022-02-20 15:46:40 +0330

    - Wrote a test with WebTestClient and @AutoConfigureWebTestClient

---
+++ microservices2/first/microservices/product-composite-service/src/test/java/ir/darkdeveloper/composite/product/services/ProductCompositeServiceImplTest.java
@@ -0,0 +1,39 @@
+package ir.darkdeveloper.composite.product.services;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureWebTestClient
+record ProductCompositeServiceImplTest(WebTestClient client) {
+
+    private static final Integer productId = 5;
+
+    @Autowired
+    public ProductCompositeServiceImplTest {
+    }
+
+    @Test
+    @Order(1)
+    void getProduct() {
+        // other microservices should be available(online and running)
+        client.get()
+                .uri("/product-composite/" + productId)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.productId").isEqualTo(productId)
+                .jsonPath("$.recommendations.length()").isEqualTo(1)
+                .jsonPath("$.reviews.length()").isEqualTo(1);
+    }
+}
--- microservices2/first/util/src/main/java/ir/darkdeveloper/microservice/util/http/GlobalControllerExceptionHandler.java
+++ microservices2/first/util/src/main/java/ir/darkdeveloper/microservice/util/http/GlobalControllerExceptionHandler.java
@@ -18,31 +18,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 class GlobalControllerExceptionHandler {
 
-  private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
-  @ResponseStatus(NOT_FOUND)
-  @ExceptionHandler(NotFoundException.class)
-  public @ResponseBody HttpErrorInfo handleNotFoundExceptions(
-    ServerHttpRequest request, NotFoundException ex) {
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public @ResponseBody
+    HttpErrorInfo handleNotFoundExceptions(ServerHttpRequest request,
+                                           NotFoundException ex) {
+        return createHttpErrorInfo(NOT_FOUND, request, ex);
+    }
 
-    return createHttpErrorInfo(NOT_FOUND, request, ex);
-  }
+    @ResponseStatus(UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(InvalidInputException.class)
+    public @ResponseBody
+    HttpErrorInfo handleInvalidInputException(ServerHttpRequest request,
+                                              InvalidInputException ex) {
+        return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
+    }
 
-  @ResponseStatus(UNPROCESSABLE_ENTITY)
-  @ExceptionHandler(InvalidInputException.class)
-  public @ResponseBody HttpErrorInfo handleInvalidInputException(
-    ServerHttpRequest request, InvalidInputException ex) {
+    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, ServerHttpRequest request,
+                                              Exception ex) {
+        final String path = request.getPath().pathWithinApplication().value();
+        final String message = ex.getMessage();
 
-    return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
-  }
-
-  private HttpErrorInfo createHttpErrorInfo(
-    HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
-
-    final String path = request.getPath().pathWithinApplication().value();
-    final String message = ex.getMessage();
-
-    LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
-    return new HttpErrorInfo(httpStatus, path, message);
-  }
+        LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
+        return new HttpErrorInfo(httpStatus, path, message);
+    }
 }
