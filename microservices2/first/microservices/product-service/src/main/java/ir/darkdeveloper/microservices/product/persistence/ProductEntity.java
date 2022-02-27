a0cc6dda9eeec219aa5239ac5651c67a14701356
DarkDeveloper <goldeagle00110011@outlook.com>
2022-02-27 23:38:47 +0330

    - Added persistence deps - Added ProductEntity

---
+++ microservices2/first/microservices/product-service/src/main/java/ir/darkdeveloper/microservices/product/persistence/ProductEntity.java
@@ -0,0 +1,23 @@
+package ir.darkdeveloper.microservices.product.persistence;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "products")
+public class ProductEntity {
+
+    @Id
+    private String id;
+
+    @Version
+    private Integer version;
+
+    @Indexed(unique = true)
+    private int productId;
+
+    private String name;
+    private int weight;
+
+}
